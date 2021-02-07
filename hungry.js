9a9c0ad2975d47cab10d9a987e9ab71badc27bc3
Rishad05 <76250270+Rishad05@users.noreply.github.com>
2021-02-07 20:59:44 +0600

    final adding code

--- hungry.js
+++ hungry.js
@@ -1,7 +1,7 @@
  const searchBtn = document.getElementById('search-btn');
 
  searchBtn.addEventListener('click', () => {
-     const mealInput = document.getElementById('meal-name').value.trim();
+     const mealInput = document.getElementById('meal-name').value;
 
      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}`)
          .then(res => res.json())
@@ -12,11 +12,32 @@
      let mealName = "";
      if (data.meals) {
          data.meals.forEach(meal => {
-             mealName = mealName + `<div class="meal"><img src = "${meal.strMealThumb}" alt = "meal">
+             mealName = mealName + `<div onclick="displayMealDetails('${meal.idMeal}')" class="meal"><img src = "${meal.strMealThumb}" alt = "meal">
                      <h3>${meal.strMeal}</h3></div>`;
          });
      } else {
          mealName = "!!There is no meal!!";
      }
      mealsDiv.innerHTML = mealName;
+ }
+ const displayMealDetails = mealInput => {
+     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealInput}`)
+         .then(res => res.json())
+         .then(data => {
+             renderIngredient(data.meals[0]);
+         })
+ }
+ const renderIngredient = meal => {
+     const ingredientContainer = document.getElementById("ingredientList");
+     ingredientContainer.innerHTML = `<img id="image" src="${meal.strMealThumb}">
+<h1> ${meal.strMeal}</h1>
+<h3>ingredients</h3>
+<p>${meal.strMeasure1}${meal.strIngredient1}</p>
+<p>${meal.strMeasure2}${meal.strIngredient2}</p>
+<p>${meal.strMeasure3}${meal.strIngredient3}</p>
+<p>${meal.strMeasure4}${meal.strIngredient4}</p>
+<p>${meal.strMeasure5}${meal.strIngredient5}</p>
+<p>${meal.strMeasure7}${meal.strIngredient6}</p>
+<p>${meal.strMeasure6}${meal.strIngredient7}</p>
+<p>${meal.strMeasure7}${meal.strIngredient8}</p>`;
  }
